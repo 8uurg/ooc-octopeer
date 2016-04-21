607be310276d5b3f782248f6fea830e1ae0c31f0
8uurg <arthur.guijt12@gmail.com>
2016-04-21 08:57:59 +0200

    Set up build tool, sort of.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Octopeer for Bitbucket
+
+## Setting up / building
+Install *NodeJS* from [here](https://nodejs.org)
+
+Make sure *gulp* is globally installed, if not: install using npm:
+```
+npm install -g gulp
+```
+Install other dependencies:
+```
+npm install
+```
+
+## Gulp
+A few commands can be executed using gulp:
+- *Compile* convert typescript to javascript
+- *Test*: Run the test suite
+
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp')
+const jasmine = require('gulp-jasmine')
+const ts = require('gulp-typescript')
+const clean = require('gulp-clean')
+
+const tsv = ts.createProject({});
+const tst = ts.createProject({});
+
+gulp.task('compile', function() {
+	return gulp.src('./src/main/*.ts').pipe(ts(tsv)).pipe(gulp.dest('./target/'));
+});
+
+gulp.task('test', ['compile'], function() {
+	return gulp.src('./src/test/*.ts').pipe(ts(tst)).pipe(jasmine());
+});
+
+gulp.task('clean', function() {
+	return gulp.src('./target', {read: false}).pipe(clean());
+});
+
