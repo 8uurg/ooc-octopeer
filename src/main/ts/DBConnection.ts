59558c0e49351783d42c67fa1b04f6900c9d4ba3
CoderDS <sblyst@hotmail.com>
2016-04-25 11:01:18 +0200

    Added function which can insert a new session in the database

--- src/main/ts/DBConnection.ts
+++ src/main/ts/DBConnection.ts
@@ -3,24 +3,10 @@
  */
 class DBConnection {
 
-     sendMousePosition(url: string, startX: number, startY: number, startHTMLElement: string, endX: number, endY: number,
-                       endHTMLElement: string, timeStart: Date, timeEnd: Date, session: string) {
-
-        var urlWithParameters = "http://localhost:8000/api/mouse-movement-events?";
-        urlWithParameters += "url:" + url + "?";
-        urlWithParameters += "start_x_position:" + startX + "?";
-        urlWithParameters += "start_y_position:" + startY + "?";
-        urlWithParameters += "start_html_element:" + startHTMLElement + "?";
-        urlWithParameters += "end_x_position:" + endX + "?";
-        urlWithParameters += "end_y_position:" + endY + "?";
-        urlWithParameters += "end_html_element:" + endHTMLElement + "?";
-        urlWithParameters += "started_at:" + timeStart + "?";
-        urlWithParameters += "ended_at:" + timeEnd + "?";
-        urlWithParameters += "session:" + session;
-
-
+    sessionSetup(sessionID: string, user_hash: string) {
         var xmlHTTP = new XMLHttpRequest();
-         xmlHTTP.open("GET", urlWithParameters, true);
-         xmlHTTP.send(null);
+        xmlHTTP.open("POST", "http://localhost:8000/api/session/", true);
+        xmlHTTP.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xmlHTTP.send({"session": sessionID, "user_hash": user_hash, "platform": "bitbucket"});
     }
 }
