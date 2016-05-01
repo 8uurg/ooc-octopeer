bfaa6fade906bc34454d45b4bf1dc53068fb3e86
lkroesen <L.Kroesen@student.tudelft.nl>
2016-05-01 20:29:18 +0200

    Add semicolons.

--- src/main/js/popup.js
+++ src/main/js/popup.js
@@ -30,34 +30,34 @@ document.addEventListener("DOMContentLoaded", function() {
     var encryptCheckbox = document.getElementById("checkboxEncrypt");
     encryptCheckbox.addEventListener("click", function() {
             bEncrypt =  encryptCheckbox.checked;
-            console.log("Encrypt Tracking Data: " + bEncrypt)
+            console.log("Encrypt Tracking Data: " + bEncrypt);
     });
 
     /* Track Mouse Data Checkbox Listener */
     var mouseDataCheckbox = document.getElementById("checkboxMouseData");
     mouseDataCheckbox.addEventListener("click", function() {
             bMouseData = mouseDataCheckbox.checked;
-            console.log("Track Mouse Data: " + bMouseData)
+            console.log("Track Mouse Data: " + bMouseData);
     });
 
     /* Track Keyboard Data Checkbox Listener */
     var keyboardDataCheckbox = document.getElementById("checkboxKeyboardData");
     keyboardDataCheckbox.addEventListener("click", function() {
             bKeyboardData = keyboardDataCheckbox.checked;
-            console.log("Track Keyboard Data: " + bKeyboardData)
+            console.log("Track Keyboard Data: " + bKeyboardData);
     });
 
     /* Track PR Meta Data Checkbox Listener */
     var prMetaDataCheckbox = document.getElementById("checkboxPrMetaData");
     prMetaDataCheckbox.addEventListener("click", function() {
             bPrMetaData = prMetaDataCheckbox.checked;
-            console.log("Track PR Meta Data: " + bPrMetaData)
+            console.log("Track PR Meta Data: " + bPrMetaData);
     });
 
     /* Track Browser Data Checkbox Listener */
     var browserDataCheckbox = document.getElementById("checkboxBrowserData");
     browserDataCheckbox.addEventListener("click", function () {
             bBrowserData = browserDataCheckbox.checked;
-            console.log("Track Browser Data: " + bBrowserData)
+            console.log("Track Browser Data: " + bBrowserData);
     });
 });
