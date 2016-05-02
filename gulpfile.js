0b2b8d4bdbab6fa29bb61e39aab085ea0be7b439
8uurg <arthur.guijt12@gmail.com>
2016-05-02 17:41:24 +0200

    Remap to typescript

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const util = require('gulp-util');
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
 const lazypipe = require('lazypipe');
+const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 
 
 const fail = function() {
@@ -41,24 +42,57 @@ gulp.task('compile', ['clean'], function() {
            .pipe(gulp.dest('./target/src'));
 });
 
-gulp.task('test', ['compile'], function(done) {
+gulp.task('test-prepare', ['compile'], function() {
     return gulp.src(['./target/src/*.js', './target/src/**/*.js'])
         .pipe(istanbul({includeUntested: true}))
         .pipe(istanbul.hookRequire())
-        .on('end', function() { 
-            gulp.src(['./target/src/test/**/*.js', './target/src/js/test/*.js'])
-                 .pipe(jasmine())
-                 .pipe(istanbul.writeReports({
-                    dir: './target/assets/unit-test-coverage',
-                    reporters: [ 'lcov' ],
-                    reportOpts: { dir: './target/assets/unit-test-coverage'}
-                 }));
-        });
 });
 
-gulp.task('run-test', function() {
-    return gulp.src(['./target/src/test/**/*.js', './src/target/js/test/*.js']).pipe(jasmine());
+gulp.task('test-run', ['test-prepare'], function() {
+    return gulp.src(['./target/src/test/**/*.js', './target/src/js/test/*.js'])
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports({
+            dir: './target/assets/unit-test-coverage',
+            reporters: [ 'json' ], // Report json to transform
+            reportOpts: { dir: './target/assets/unit-test-coverage'}
+        }));
 });
+
+gulp.task('test-report', ['test-run'], function() {
+    return gulp.src("./target/assets/unit-test-coverage/coverage-final.json")
+        .pipe(remapIstanbul({
+            reports: {
+                'json': './target/assets/unit-test-coverage/coverage.json',
+                'html': './target/assets/unit-test-coverage/html-report'
+            }
+        }));
+})
+
+gulp.task('test', ['test-report']); 
+
+// gulp.task('test', ['compile'], function(done) {
+//     return gulp.src(['./target/src/*.js', './target/src/**/*.js'])
+//         .pipe(istanbul({includeUntested: true}))
+//         .pipe(istanbul.hookRequire())
+//         .on('end', function() { 
+//             gulp.src(['./target/src/test/**/*.js', './target/src/js/test/*.js'])
+//                  .pipe(jasmine())
+//                  .pipe(istanbul.writeReports({
+//                     dir: './target/assets/unit-test-coverage',
+//                     reporters: [ 'json' ], // Report json to transform
+//                     reportOpts: { dir: './target/assets/unit-test-coverage'}
+//                  })).on('end', function () {
+//                     // Convert coverage from js to ts.
+//                     gulp.src("./target/assets/unit-test-coverage/coverage-final.json").pipe(remapIstanbul({
+//                         reports: {
+//                             'json': './target/assets/unit-test-coverage/coverage.json',
+//                             'html': './target/assets/unit-test-coverage/html-report'
+//                         }
+//                     }));
+//             });
+//         });
+// });
+
 
 gulp.task('build', ['test'], function() {
     return gulp.src('./target/src/main/**').pipe(gulp.dest('./dest'))
