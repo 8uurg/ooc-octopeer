991da9948b207c4e0868e835cf7b7c465ecccf2b
8uurg <arthur.guijt12@gmail.com>
2016-05-08 15:34:28 +0200

    Add some extra testing for RARequests.
    Note that I used global here to fake the existance of XMLHttpRequest.
    We would've mocked it anyways.

--- src/test/TestRARequests.ts
+++ src/test/TestRARequests.ts
@@ -4,6 +4,7 @@ import {RARequests} from '../main/js/RARequests';
 /**
  * Created by Cas on 8-5-2016.
  */
+declare var global: any;
 
 describe('RESTFul API requests', () => {
 
@@ -27,10 +28,39 @@ describe('RESTFul API requests', () => {
     });
 
     it('should have an api_location set', function() {
-
         var rarObject = new RARequests(null);
         rarObject.sendUserName({"url":"someURL", "username":"someUsername"});
 
         expect(rarObject.getSend()).toEqual(false);
+    });
+    
+    it('should send the request, and set send to true if it succeeds.', function() {
+        //Fake the XMLHttpRequest object so that the test can run.
+        global.XMLHttpRequest = function() {
+            this.open = function() {};
+            this.setRequestHeader = function() {};
+            this.send = function() {};
+            this.status = 200;
+        }
+        
+        var rarObject = new RARequests("Test");
+        rarObject.sendUserName({"url":"someURL", "username":"someUsername"});
+
+        expect(rarObject.getSend()).toBeTruthy();
+    });
+    
+    it('should send the request, and keep send at false.', function() {
+        //Fake the XMLHttpRequest object so that the test can run.
+        global.XMLHttpRequest = function() {
+            this.open = function() {};
+            this.setRequestHeader = function() {};
+            this.send = function() {};
+            this.status = 201;
+        }
+        
+        var rarObject = new RARequests("Test");
+        rarObject.sendUserName({"url":"someURL", "username":"someUsername"});
+
+        expect(rarObject.getSend()).toBeFalsy();
     });
 });
