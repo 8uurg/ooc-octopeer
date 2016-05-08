2f8629ef74547ccf3e4fedc04365b334a1bc7413
8uurg <arthur.guijt12@gmail.com>
2016-05-08 22:17:33 +0200

    Style changes
    As requested by Cas.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const gulp = require('gulp');
-const jasmine = require('gulp-jasmine');
-const istanbul = require('gulp-istanbul');
-const ts = require('gulp-typescript');
-const clean = require('gulp-clean');
-const util = require('gulp-util');
-const gulpif = require('gulp-if');
-const sourcemaps = require('gulp-sourcemaps');
-const lazypipe = require('lazypipe');
+const gulp          = require('gulp');
+const jasmine       = require('gulp-jasmine');
+const istanbul      = require('gulp-istanbul');
+const ts            = require('gulp-typescript');
+const clean         = require('gulp-clean');
+const util          = require('gulp-util');
+const gulpif        = require('gulp-if');
+const sourcemaps    = require('gulp-sourcemaps');
+const lazypipe      = require('lazypipe');
 const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
-const tslint = require('gulp-tslint');
+const tslint        = require('gulp-tslint');
 
 
 const fail = function() {
@@ -22,11 +22,29 @@ const tsv = ts.createProject({
     module: 'commonjs',
     noImplicitAny: true
 });
+
 const tst = ts.createProject({
     noEmitOnError: true,
     target: 'es6',
     module: 'commonjs',
     noImplicitAny: true
+});
+
+const compile_ts = lazypipe()
+    .pipe(sourcemaps.init)
+    .pipe(function() { 
+        return ts(tsv).on('error', fail); })
+    .pipe(sourcemaps.write, './maps');
+    
+const sourcemap_js = lazypipe()
+    .pipe(sourcemaps.init, {identityMap: true}) // identityMap is turned on because of how the coverage remapper works.
+    .pipe(sourcemaps.write, './maps');
+
+gulp.task('compile', ['clean'], function() {
+    return gulp.src('./src/**')
+           .pipe(gulpif('**/*.ts', compile_ts()))
+           .pipe(gulpif('**/*.js', sourcemap_js()))
+           .pipe(gulp.dest('./target/src'));
 });
 
 gulp.task('lint', function() {
@@ -36,23 +54,6 @@ gulp.task('lint', function() {
     .pipe(tslint.report("prose", {
           emitError: false
         }));
-});
-
-const compile_ts = lazypipe()
-    .pipe(sourcemaps.init)
-    .pipe(function() { 
-        return ts(tsv).on('error', fail); })
-    .pipe(sourcemaps.write, './maps');
-    
- const sourcemap_js = lazypipe()
-    .pipe(sourcemaps.init, {identityMap: true}) // identityMap is turned on because of how the coverage remapper works.
-    .pipe(sourcemaps.write, './maps');
-
-gulp.task('compile', ['clean'], function() {
-    return gulp.src('./src/**')
-           .pipe(gulpif('**/*.ts', compile_ts()))
-           .pipe(gulpif('**/*.js', sourcemap_js()))
-           .pipe(gulp.dest('./target/src'));
 });
 
 gulp.task('test-prepare', ['compile'], function() {
