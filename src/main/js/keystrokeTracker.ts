b0d3532b373006c5b9f3ee1f8e1ca02a1fd2eb37
Thomas Overklift <t.a.r.overkliftvaupelklein@student.tudelft.nl>
2016-05-10 19:18:55 +0200

    Made the keylogger more test friendly.

--- src/main/js/keystrokeTracker.ts
+++ src/main/js/keystrokeTracker.ts
@@ -1,62 +1,92 @@
-var keyCode: number = null;
-var keyName: string = null;
+export class keystrokeTracker {
 
-/**
- * Create an EventListener that fires each time a key is pressed. Log the key that is pressed in the console.
- * @param event object that contains the required key information.
- */
-document.addEventListener('keydown', function(event) {
-    keyCode = event.keyCode;
+    private keyCode: number = 0;
+    private keyName: string = "";
 
-    switch(keyCode) {
-        case  8:
-            keyName = "[Backspace]";
-            break;
-        case  9:
-            keyName = "[Tab]";
-            break;
-        case 13:
-            keyName = "[Enter]";
-            break;
-        case 16:
-            keyName = "[Shift]";
-            break;
-        case 17:
-            keyName = "[Control]";
-            break;
-        case 18:
-            keyName = "[Alt]";
-            break;
-        case 20:
-            keyName = "[Caps Lock]";
-            break;
-        case 27:
-            keyName = "[Escape]";
-            break;
-        case 32:
-            keyName = "[Space]";
-            break;
-        case 33:
-            keyName = "[Page Up]";
-            break;
-        case 34:
-            keyName = "[Page Down]";
-            break;
-        case 35:
-            keyName = "[End]";
-            break;
-        case 36:
-            keyName = "[Home]";
-            break;
-        case 45:
-            keyName = "[Insert]";
-            break;
-        case 46:
-            keyName = "[Delete]";
-            break;
-        default:
-            keyName = String.fromCharCode(keyCode);
+    /**
+     * Register the keystroke tracker.
+     */
+    public register() {
+        var current: keystrokeTracker = this;
+
+        /**
+         * Create an EventListener that fires each time a key is pressed. Log the key that is pressed in the console.
+         * @param event object that contains the required key information.
+         */
+        document.addEventListener('keydown', function (event) {
+            current.keyCode = event.keyCode;
+
+            switch (current.keyCode) {
+                case  8:
+                    current.keyName = "[Backspace]";
+                    break;
+                case  9:
+                    current.keyName = "[Tab]";
+                    break;
+                case 13:
+                    current.keyName = "[Enter]";
+                    break;
+                case 16:
+                    current.keyName = "[Shift]";
+                    break;
+                case 17:
+                    current.keyName = "[Control]";
+                    break;
+                case 18:
+                    current.keyName = "[Alt]";
+                    break;
+                case 20:
+                    current.keyName = "[Caps Lock]";
+                    break;
+                case 27:
+                    current.keyName = "[Escape]";
+                    break;
+                case 32:
+                    current.keyName = "[Space]";
+                    break;
+                case 33:
+                    current.keyName = "[Page Up]";
+                    break;
+                case 34:
+                    current.keyName = "[Page Down]";
+                    break;
+                case 35:
+                    current.keyName = "[End]";
+                    break;
+                case 36:
+                    current.keyName = "[Home]";
+                    break;
+                case 45:
+                    current.keyName = "[Insert]";
+                    break;
+                case 46:
+                    current.keyName = "[Delete]";
+                    break;
+                default:
+                    current.keyName = String.fromCharCode(current.keyCode);
+            }
+
+            current.logKeystroke();
+        });
     }
-    
-    console.log("Keystroke: " + keyName);
-});
+
+    /**
+     * Log the key that has been pressed and send it to the database.
+     */
+    logKeystroke() {
+        this.sendData(this.keyName);
+        console.log("Keystroke: " + this.keyName);
+    }
+
+    /**
+     * Send data to the database
+     * @param keyName name of the key that has been pressed.
+     */
+    sendData(keyName: string) {
+        //TODO: implement this
+    }
+}
+
+//Create an instance of the keystroke tracker.
+var tracker = new keystrokeTracker();
+tracker.register();
