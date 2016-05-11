64c222a82be94c10831d3c5b12dda36ffa4e80f0
8uurg <arthur.guijt12@gmail.com>
2016-05-11 19:19:41 +0200

    Fix tests for new behavior.

--- src/test/testResizeTracker.ts
+++ src/test/testResizeTracker.ts
@@ -11,15 +11,21 @@ describe("The ResizeTracker", function() {
         jasmine.clock().install();
         this.tracker = new ResizeTracker();
         this.ev = <(e: any) => void> null;
-        let here = this;
+        let _this = this;
         global.window = {
             addEventListener: function(eventName: string, callback: (e: any) => void) {
-                here.ev = callback;
+                _this.ev = callback;
             },
             innerWidth: 400,
             innerHeight: 500
         };
        this.tracker.register();
+
+       // Mock current date for repeatable tests. 
+       _this.date = 0;
+       spyOn(Date, "now").and.callFake(function () {
+           return _this.date;
+       });
     });
 
     it("should not start sending without being triggered first.", function() {
@@ -28,11 +34,10 @@ describe("The ResizeTracker", function() {
        expect(this.ev).not.toHaveBeenCalled();
     });
 
-    xit("should send a resize if the screen got resized.", function() {
+    it("should send a resize if the screen got resized.", function() {
        spyOn(this.tracker, "sendData");
-       this.ev({
-           timeStamp: 450
-       });
+       this.date = 450;
+       this.ev();
        jasmine.clock().tick(400);
        expect(this.tracker.sendData).toHaveBeenCalledWith({
            width: 400,
@@ -41,17 +46,35 @@ describe("The ResizeTracker", function() {
        });
     });
 
-    xit("should not send all resize events during a resize.", function() {
+    it("should not send all resize events during a resize.", function() {
        spyOn(this.tracker, "sendData");
-       this.ev({
-           timeStamp: 450
-       });
+       this.date = 450;
+       this.ev();
        jasmine.clock().tick(40);
-       this.ev({
-           timeStamp: 459
-       });
+       this.date = 459;
+       this.ev();
        jasmine.clock().tick(400);
        expect(this.tracker.sendData).not.toHaveBeenCalledWith({
+           width: 400,
+           height: 500,
+           timestamp: 450
+       });
+       expect(this.tracker.sendData).toHaveBeenCalledWith({
+           width: 400,
+           height: 500,
+           timestamp: 459
+       });
+    });
+
+    it("should send all resize events if they are different resizes.", function() {
+       spyOn(this.tracker, "sendData");
+       this.date = 450;
+       this.ev();
+       jasmine.clock().tick(3000);
+       this.date = 459;
+       this.ev();
+       jasmine.clock().tick(400);
+       expect(this.tracker.sendData).toHaveBeenCalledWith({
            width: 400,
            height: 500,
            timestamp: 450
