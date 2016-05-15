2edcab1b2634ae9e668c2aecdd195350ead6c1be
lkroesen <L.Kroesen@student.tudelft.nl>
2016-05-15 14:15:11 +0200

    Add interface for KeystrokeJSON.

---
+++ src/main/js/interfaces/KeystrokeJSON.ts
@@ -0,0 +1,25 @@
+/**
+ * This interface is used to define the structure for keystrokes accepted by the RESTFul API.
+ */
+
+interface KeystrokeJSON {
+    /**
+     * The url of the page.
+     */
+    url: string;
+
+    /**
+     * The timestamp of when the keystroke was logged.
+     */
+    created_at: Date;
+
+    /**
+     * The key name of the keystroke that was logged.
+     */
+    keyName: string
+
+    /**
+     * The link to the session of the PR of this user.
+     */
+    session: string;
+}
