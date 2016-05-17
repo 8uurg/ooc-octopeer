4e4dc7360180b78552c23334522fecd30f85d5a0
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-05-17 18:59:57 +0200

    Apply the settings to the main file
    The needed settings variable contains the default values, which are used if the key is not found in chrome storage. We also use chrome.storage.sync, so the settings are applied across all Chrome installations of an user.

--- src/main/js/main.ts
+++ src/main/js/main.ts
@@ -11,16 +11,30 @@ declare var UserIdTracker: any;
  * Created by larsstegman on 16-05-16.
  * This file is the starting point for each BitBucket page tracking.
  */
+let neededSettings: { [key: string]: boolean; } = {
+    [OCTOPEER_CONSTANTS.track_key_strokes]: true,
+    [OCTOPEER_CONSTANTS.track_mouse_position]: true,
+    [OCTOPEER_CONSTANTS.track_page_resolution]: true
+};
 
-// Log the current user name and repository id.
-(new UserIdTracker()).log();
+let syncedStorage = chrome.storage.sync;
+syncedStorage.get(neededSettings, (items: { [key: string]: any }) => {
+    // Log the current user name and repository id.
+    (new UserIdTracker()).log();
 
-// Create an instance of the keystroke tracker.
-(new KeystrokeTracker).register();
+    // Create an instance of the keystroke tracker.
+    if (items[OCTOPEER_CONSTANTS.track_key_strokes]) {
+        (new KeystrokeTracker()).register();
+    }
 
-// Register the mousetracker to the current document.
-(new MouseTracker()).register();
+    // Register the mousetracker to the current document.
+    if (items[OCTOPEER_CONSTANTS.track_mouse_position]) {
+        (new MouseTracker()).register();
+    }
 
-// Register the resize tracker to the current document.
-(new ResizeTracker()).register();
+    // Register the resize tracker to the current document.
+    if (items[OCTOPEER_CONSTANTS.track_page_resolution]) {
+        (new ResizeTracker()).register();
+    }
+});
 
