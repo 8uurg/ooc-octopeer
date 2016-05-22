ed329ba4ff9940845ad1e93fa6d8d5ad3b472172
lkroesen <L.Kroesen@student.tudelft.nl>
2016-05-22 13:59:42 +0200

    Add implementation of Strategy for sending.

--- src/main/js/trackers/MousePositionTracker.ts
+++ src/main/js/trackers/MousePositionTracker.ts
@@ -1,7 +1,9 @@
 /**
  * Provides a tracker that tracks the mouse on the webpage.
  */
-export class MousePositionTracker {
+export class MousePositionTracker implements Sender {
+    private sendStrategy: SendStrategy;
+    private messageDelay: number = 1000;
     private port: any;
     private cursorX: number = -1;
     private cursorY: number = -1;
@@ -11,11 +13,12 @@ export class MousePositionTracker {
 
     /**
      * Register the mouse tracker to the document.
+     * @param sendStrategy Set a sendStrategy for the tracker to use.
      */
-    public register() {
+    public register(sendStrategy: SendStrategy) {
         // Store `this` for usage in functions.
         const _this: MousePositionTracker = this;
-
+        _this.sendStrategy = sendStrategy;
         _this.port = chrome.runtime.connect({name: "requestSender"});
 
         /**
@@ -27,7 +30,12 @@ export class MousePositionTracker {
             _this.cursorY = event.pageY;
             _this.viewportX = event.clientX;
             _this.viewportY = event.clientY;
-            _this.sendData();
+
+            if (_this.sendStrategy === SendStrategy.Bulk) {
+                _this.sendData();
+            } else {
+                _this.sendThrottledData();
+            }
         });
     }
 
@@ -35,21 +43,36 @@ export class MousePositionTracker {
      * Send data to centralized collector.
      */
     private sendData() {
+        this.port.postMessage({
+            table: "mouse-position-events/",
+            data: {
+                position_x: this.cursorX,
+                position_y: this.cursorY,
+                viewport_x: this.viewportX,
+                viewport_y: this.viewportY,
+                session: "", // Empty for now.
+                created_at: Date.now()
+            }
+        });
+    }
+
+    /**
+     * Send data once every messageDelay to centralized collector.
+     */
+    private sendThrottledData() {
         let newCall: number = Date.now();
 
-        if ( newCall - this.lastCall >= 1000 ) {
+        if ( newCall - this.lastCall >= this.messageDelay ) {
             this.lastCall = newCall;
-            this.port.postMessage({
-                table: "mouse-position-events/",
-                data: {
-                    position_x: this.cursorX,
-                    position_y: this.cursorY,
-                    viewport_x: this.viewportX,
-                    viewport_y: this.viewportY,
-                    session: "", // Empty for now.
-                    created_at: Date.now()
-                }
-            });
+            this.sendData();
         }
+    }
+
+    /**
+     * Set the time between messages, when throttled.
+     * @param messageDelay Give a number with the time between messages.
+     */
+    setMessageDelay(messageDelay:number) {
+        this.messageDelay = messageDelay;
     }
 }
