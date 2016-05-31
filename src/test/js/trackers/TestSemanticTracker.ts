692219f47cdfc9f9fb465ae196a1832b511aaccc
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-05-31 19:00:08 +0200

    Create a test for the case that only a key up event is registered.

--- src/test/js/trackers/TestSemanticTracker.ts
+++ src/test/js/trackers/TestSemanticTracker.ts
@@ -38,5 +38,33 @@ describe("The key stroke semantic tracker", () => {
         }));
     });
 
-    
+    it("should no longer register a time longer than 1 after one keydown and 2 keyups", () => {
+        let eventListenerClosureKeydown: (event: { keyCode: number }) => void = null;
+        let eventListenerClosureKeyup:   (event: { keyCode: number }) => void = null;
+        htmlElement.addEventListener.and.callFake((eventString: string, callback: any) => {
+            switch (eventString) {
+                case "keydown": eventListenerClosureKeydown = callback; break;
+                case "keyup": eventListenerClosureKeyup = callback; break;
+                default: break;
+            }
+        });
+
+        semanticTracker.registerKeystroke("Inline Comment", htmlElement);
+
+        eventListenerClosureKeydown({ keyCode: 42 });
+        let start = Date.now();
+        jasmine.clock().tick(4000);
+        eventListenerClosureKeyup({ keyCode: 42 });
+        let end = Date.now();
+        jasmine.clock().tick(4000);
+        eventListenerClosureKeyup({ keyCode: 42 });
+
+        expect(collector.sendMessage).toHaveBeenCalledTimes(2);
+        expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
+            data: jasmine.objectContaining({ duration: 1 })
+        }));
+        expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
+            data: jasmine.objectContaining({ duration: end - start })
+        }));
+    });
 });
