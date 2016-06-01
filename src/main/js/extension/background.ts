7750302b16d1af1b3802bfe5ce98dbdc7953313a
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-06-01 19:10:14 +0200

    Refactor some code

--- src/main/js/extension/background.ts
+++ src/main/js/extension/background.ts
@@ -5,32 +5,41 @@ chrome.browserAction.onClicked.addListener(() => {
     chrome.tabs.create({ url: "../../html/settings.html" });
 });
 
+let urlWithIcon = [
+    {
+        url: "bitbucket",
+        path: {
+            "48" : "../../img/icons/icon_active48.png",
+            "64" : "../../img/icons/icon_active64.png",
+            "128" : "../../img/icons/icon_active128.png"
+        }
+    },
+    {
+        url: "chrome-extension",
+        path: {
+            "48" : "../../img/icons/icon48.png",
+            "64" : "../../img/icons/icon64.png",
+            "128" : "../../img/icons/icon128.png"
+        }
+    },
+    {
+        url: "", // Is always in a string.
+        path: {
+            "48" : "../../img/icons/icon_inactive48.png",
+            "64" : "../../img/icons/icon_inactive64.png",
+            "128" : "../../img/icons/icon_inactive128.png"
+        }
+    }
+];
+
 chrome.tabs.onActivated.addListener((tabInfo) => {
     chrome.tabs.get(tabInfo.tabId, (tab) => {
-        if (tab.url.indexOf("bitbucket") !== -1) {
-            chrome.browserAction.setIcon({
-                path : {
-                    "48" : "../../img/icons/icon_active48.png",
-                    "64" : "../../img/icons/icon_active64.png",
-                    "128" : "../../img/icons/icon_active128.png"
-                }
-            });
-        } else if (tab.url.indexOf("chrome-extension") !== -1 ) {
-            chrome.browserAction.setIcon({
-                path : {
-                    "48" : "../../img/icons/icon48.png",
-                    "64" : "../../img/icons/icon64.png",
-                    "128" : "../../img/icons/icon128.png"
-                }
-            });
-        } else {
-            chrome.browserAction.setIcon({
-                path : {
-                    "48" : "../../img/icons/icon_inactive48.png",
-                    "64" : "../../img/icons/icon_inactive64.png",
-                    "128" : "../../img/icons/icon_inactive128.png"
-                }
-            });
+        for (let i = 0; i < urlWithIcon.length; i++) {
+            let urlAndPath = urlWithIcon[i];
+            if (tab.url.indexOf(urlAndPath.url) !== -1) {
+                chrome.browserAction.setIcon({ path: urlAndPath.path });
+                break;
+            }
         }
     });
 });
