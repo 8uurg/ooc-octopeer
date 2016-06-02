20522c345262ca8ea1d76f5d0db443673e63d1f7
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-06-02 12:11:56 +0200

    Replace switches in tests with an object

--- src/test/js/trackers/TestSemanticTracker.ts
+++ src/test/js/trackers/TestSemanticTracker.ts
@@ -10,6 +10,7 @@ describe("The key stroke semantic tracker", function() {
     let collector: TrackingCollector;
     let semanticTracker: SemanticTracker;
     let htmlElement: any; // Using the real type, HTMLElement, causes issues when spying on it.
+    let fireEvent: any;
 
     beforeEach(function() {
         jasmine.clock().install();
@@ -17,6 +18,10 @@ describe("The key stroke semantic tracker", function() {
         collector = jasmine.createSpyObj("collector", ["sendMessage"]);
         semanticTracker = new SemanticTracker().withCollector(collector);
         htmlElement = jasmine.createSpyObj("elem", ["addEventListener"]);
+        fireEvent = {};
+        htmlElement.addEventListener.and.callFake((eventString: string, fireEventFunction: any) => {
+            fireEvent[eventString] = fireEventFunction;
+        });
     });
 
     afterEach(function() {
@@ -27,14 +32,10 @@ describe("The key stroke semantic tracker", function() {
      * A keydown event might not have been received, e.g. when using shortcuts to change the active tab/program.
      */
     it("should make an event with duration 1, when no keydown event was registered, but a " +
-        "keyup event was registered.", function() {
-        htmlElement.addEventListener.and.callFake((eventString: string, fireEvent: any) => {
-            if (eventString === "keyup") {
-                fireEvent({ keyCode: 42 });
-            }
-        });
-
+            "keyup event was registered.", function() {
         semanticTracker.registerKeystroke("Inline Comment", htmlElement);
+
+        fireEvent["keyup"]({ keyCode: 42 });
 
         expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
             data: jasmine.objectContaining({ duration: 1 })
@@ -42,26 +43,16 @@ describe("The key stroke semantic tracker", function() {
     });
 
     it("should register the second key up event with a duration of 1, when the first keydown event has " +
-        "been followed by a key up event.", function() {
-        let eventListenerClosureKeydown: (event: { keyCode: number }) => void = null;
-        let eventListenerClosureKeyup:   (event: { keyCode: number }) => void = null;
-        htmlElement.addEventListener.and.callFake((eventString: string, fireEvent: any) => {
-            switch (eventString) {
-                case "keydown": eventListenerClosureKeydown = fireEvent; break;
-                case "keyup": eventListenerClosureKeyup = fireEvent; break;
-                default: break;
-            }
-        });
-
+            "been followed by a key up event.", function() {
         semanticTracker.registerKeystroke("Inline Comment", htmlElement);
 
-        eventListenerClosureKeydown({ keyCode: 42 });
+        fireEvent["keydown"]({ keyCode: 42 });
         let start = Date.now();
         jasmine.clock().tick(4000);
-        eventListenerClosureKeyup({ keyCode: 42 });
+        fireEvent["keyup"]({ keyCode: 42 });
         let end = Date.now();
         jasmine.clock().tick(4000);
-        eventListenerClosureKeyup({ keyCode: 42 });
+        fireEvent["keyup"]({ keyCode: 42 });
 
         expect(collector.sendMessage).toHaveBeenCalledTimes(2);
         expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
@@ -74,36 +65,22 @@ describe("The key stroke semantic tracker", function() {
 
     it("shouldn't send any data after a keydown event is registered, but before a key up event " +
             "is registered.", function() {
-        htmlElement.addEventListener.and.callFake((eventString: string, fireEvent: any) => {
-            if ( eventString === "keydown" ) {
-                fireEvent({ keyCode : 42 });
-            }
-        });
-
         semanticTracker.registerKeystroke("Inline Comment", htmlElement);
+
+        fireEvent["keydown"]({ keyCode: 42 });
         expect(collector.sendMessage).not.toHaveBeenCalled();
     });
 
     it("should only register a duplicate keystroke if both have sent a keyup event.", function() {
-        let eventListenerClosureKeydown: (event: { keyCode: number }) => void = null;
-        let eventListenerClosureKeyup:   (event: { keyCode: number }) => void = null;
-        htmlElement.addEventListener.and.callFake((eventString: string, fireEvent: any) => {
-            switch (eventString) {
-                case "keydown": eventListenerClosureKeydown = fireEvent; break;
-                case "keyup": eventListenerClosureKeyup = fireEvent; break;
-                default: break;
-            }
-        });
-
         semanticTracker.registerKeystroke("Inline Comment", htmlElement);
 
-        eventListenerClosureKeydown({ keyCode: 42 });
+        fireEvent["keydown"]({ keyCode: 42 });
         jasmine.clock().tick(1000);
         let secondTime = Date.now();
-        eventListenerClosureKeydown({ keyCode: 42 });
+        fireEvent["keydown"]({ keyCode: 42 });
         jasmine.clock().tick(1000);
         let secondEndTime = Date.now();
-        eventListenerClosureKeyup({ keyCode: 42 });
+        fireEvent["keyup"]({ keyCode: 42 });
 
         expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
             data: jasmine.objectContaining({ duration: secondEndTime - secondTime })
@@ -111,21 +88,11 @@ describe("The key stroke semantic tracker", function() {
     });
 
     it("should prevent a key from being registered if the press is older than a certain time.", function() {
-        let eventListenerClosureKeydown: (event: { keyCode: number }) => void = null;
-        let eventListenerClosureKeyup:   (event: { keyCode: number }) => void = null;
-        htmlElement.addEventListener.and.callFake((eventString: string, fireEvent: any) => {
-            switch (eventString) {
-                case "keydown": eventListenerClosureKeydown = fireEvent; break;
-                case "keyup": eventListenerClosureKeyup = fireEvent; break;
-                default: break;
-            }
-        });
-
         semanticTracker.registerKeystroke("Inline Comment", htmlElement);
 
-        eventListenerClosureKeydown({ keyCode: 42 });
+        fireEvent["keydown"]({ keyCode: 42 });
         jasmine.clock().tick(10001);
-        eventListenerClosureKeyup({ keyCode: 42 });
+        fireEvent["keyup"]({ keyCode: 42 });
 
         expect(collector.sendMessage).toHaveBeenCalledWith(jasmine.objectContaining({
             data: jasmine.objectContaining({ duration: 1})
