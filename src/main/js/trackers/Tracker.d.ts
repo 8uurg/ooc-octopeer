32e37ec585f049e84d06b7c2a0e89d471187d13a
8uurg <arthur.guijt12@gmail.com>
2016-06-05 12:08:16 +0200

    Add abstract class and definition

---
+++ src/main/js/trackers/Tracker.d.ts
@@ -0,0 +1,26 @@
+/// <reference path="../interfaces/TrackingCollector.ts" />
+
+/**
+ * An abstract class that represents a tracker.
+ * A base for building a tracker upon.
+ * Declaration / header.
+ */
+declare abstract class Tracker {
+    private collector: TrackingCollector;
+
+    /**
+     * Register a tracker into the current environment.
+     */
+    public abstract register(): void;
+
+    /**
+     * Define the collector to use with this class.
+     */
+    public withCollector(collector: TrackingCollector): Tracker;
+
+    /**
+     * Send a message over the collector.
+     */
+    public sendMessage(message: Message): void;
+    
+} 
---
+++ src/main/js/trackers/Tracker.ts
@@ -0,0 +1,29 @@
+/// <reference path="../interfaces/TrackingCollector.ts" />
+
+/**
+ * An abstract class that represents a tracker.
+ * A base for building a tracker upon.
+ */
+export abstract class Tracker {
+    private collector: TrackingCollector;
+
+    /**
+     * Register a tracker into the current environment.
+     */
+    public abstract register(): void;
+
+    /**
+     * Define the collector to use with this class.
+     */
+    public withCollector(collector: TrackingCollector): Tracker {
+        this.collector = collector;
+        return this;
+    }
+
+    /**
+     * Send a message over the collector.
+     */
+    public sendMessage(message: Message): void {
+        this.collector.sendMessage(message);
+    }
+} 
