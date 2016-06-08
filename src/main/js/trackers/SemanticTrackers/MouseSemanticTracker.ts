e4239643ec8c987af9a14ee3096c019ff2db9a5d
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-06-08 21:16:04 +0200

    Create semantic tracker for mouse events.

---
+++ src/main/js/trackers/SemanticTrackers/MouseSemanticTracker.ts
@@ -0,0 +1,23 @@
+/// <reference path="./SemanticTracker.d.ts" />
+
+/**
+ * The semantic key stroke tracker
+ */
+export class MouseSemanticTracker
+    extends SemanticTracker {
+
+    /**
+     * Register an HTMLElement to this semantic tracker.
+     * @param element The element to monitor
+     * @param eventName The name of the event.
+     */
+    public registerElement(element: Element, eventName: string) {
+        element.addEventListener("mouseenter", () => {
+            this.sendData(this.createMessage("Mouseenter", eventName));
+        });
+
+        element.addEventListener("mouseleave", () => {
+            this.sendData(this.createMessage("Mouseleave", eventName));
+        });
+    }
+}
