586d20cbfd0fbae02ae94799b4ee6bdf3268be0e
8uurg <arthur.guijt12@gmail.com>
2016-06-08 20:01:25 +0200

    Remove trottle from Resize Tracker.

--- src/main/js/trackers/ResizeTracker.ts
+++ src/main/js/trackers/ResizeTracker.ts
@@ -8,7 +8,6 @@ export class ResizeTracker extends Tracker {
     private width: number = -1;
     private height: number = -1;
     private timestamp: number = -1;
-    private timer: any = null;
 
     /**
      * Registers and hooks the instance into the environment.
@@ -21,12 +20,7 @@ export class ResizeTracker extends Tracker {
             _this.height = window.innerHeight;
             _this.timestamp = Date.now();
 
-            // Stop the previous resize event from being sent.
-            clearTimeout(_this.timer);
-
-            _this.timer = setTimeout(function () {
-                _this.sendData(_this.createMessage());
-            }, 400);
+            _this.sendData(_this.createMessage());
         };
         // Send initial window size on page load.
         window.addEventListener("load", prepareDatapoint);
