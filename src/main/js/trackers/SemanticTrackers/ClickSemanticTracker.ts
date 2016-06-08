9be9ee921296e395d09549fc7600a55714a98265
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-06-08 21:15:48 +0200

    Create semantic tracker for click events.

---
+++ src/main/js/trackers/SemanticTrackers/ClickSemanticTracker.ts
@@ -0,0 +1,19 @@
+/// <reference path="./SemanticTracker.d.ts" />
+
+/**
+ * The semantic key stroke tracker
+ */
+export class ClickSemanticTracker
+    extends SemanticTracker {
+
+    /**
+     * Register an HTMLElement to this semantic tracker.
+     * @param element The element to monitor
+     * @param eventName The name of the event.
+     */
+    public registerElement(element: Element, eventName: string) {
+        element.addEventListener("click", () => {
+            this.sendData(this.createMessage("Click", eventName));
+        });
+    }
+}
