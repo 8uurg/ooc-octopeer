6fd47aea5032ff7838ad17d107430755a6a4c2c4
8uurg <arthur.guijt12@gmail.com>
2016-06-10 13:25:42 +0200

    Add test for isReadyToSend

--- src/test/js/trackers/throttles/TestThrottle.ts
+++ src/test/js/trackers/throttles/TestThrottle.ts
@@ -3,15 +3,24 @@
 import {Throttle} from "../../../../main/js/trackers/throttles/Throttle";
 
 describe("The basic throttle", function () {
-    it("should just pass the message onwards", function() {
-        let fakeCollector = jasmine.createSpyObj("Collector", ["sendMessage"]);
-        let throttle = new Throttle(fakeCollector);
-        let fakeMessage: any = {
+    beforeEach(function () {
+        this.fakeCollector = jasmine.createSpyObj("Collector", ["sendMessage", "isReadyToSend"]);
+        this.throttle = new Throttle(this.fakeCollector);
+        this.fakeMessage = {
             test: "isWorking"
         };
+    });
 
-        throttle.sendMessage(fakeMessage);
+    it("should just pass the message onwards", function() {
+        this.throttle.sendMessage(this.fakeMessage);
 
-        expect(fakeCollector.sendMessage).toHaveBeenCalledWith(fakeMessage);
+        expect(this.fakeCollector.sendMessage).toHaveBeenCalledWith(this.fakeMessage);
+    });
+
+    it("should pass the request if it ready to send onwards.", function () {
+        this.fakeCollector.isReadyToSend.and.returnValues(false, false, true);
+        expect(this.throttle.isReadyToSend()).toBeFalsy();
+        expect(this.throttle.isReadyToSend()).toBeFalsy();
+        expect(this.throttle.isReadyToSend()).toBeTruthy();
     });
 });
