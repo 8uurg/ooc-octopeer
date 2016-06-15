11a7203d679b36b869b38218c056c46683b0587c
8uurg <arthur.guijt12@gmail.com>
2016-06-15 12:08:52 +0200

    Main-ify VisibilityTracker

--- src/main/js/trackers/RawDataTrackers/VisibilityTracker.ts
+++ src/main/js/trackers/RawDataTrackers/VisibilityTracker.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../interfaces/DatabaseSchemes/SemanticEventJSON.ts" />
 /// <reference path="./Tracker.d.ts" />
+/// <reference path="../../Main.d.ts" />
+declare var OCTOPEER_CONSTANTS;
 
 /**
  * Provides a tracker that tracks the visibility on the page.
@@ -46,3 +48,14 @@ export class VisibilityTracker extends Tracker {
         });
     }
 }
+
+main.declareTracker({
+    tracker: (collector) => { 
+        return (new VisibilityTracker())
+            .withCollector(collector);
+    },
+    setting: {
+        name: OCTOPEER_CONSTANTS.track_key_strokes,
+        def: true
+    }
+});
